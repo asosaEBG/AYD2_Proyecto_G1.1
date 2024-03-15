e411bb678f4545aceb092d9c6d487cf259a473a7
Jorge <jorgeperez@inlandlogistics.co>
2024-03-15 10:34:59 -0600

    [ADD] componente set password

--- frontend/src/app/auth/auth.routes.ts
+++ frontend/src/app/auth/auth.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
+import { SetPasswordComponent } from "./set-password/set-password.component";
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   { path: "forgot-password", component: ForgotPasswordComponent },
+  { path: "set-password", component: SetPasswordComponent },
   { path: "**", redirectTo: "login" },
 ];
 
