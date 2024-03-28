359141af7e1f5673c488b9575c1a19366d523d3f
asosaEBG <asosa@ebg.edu.gt>
2024-03-27 23:08:27 -0600

    [UPDATE] JWT for protecting routes

--- backend/src/routes/admin/admin.js
+++ backend/src/routes/admin/admin.js
@@ -1,15 +1,14 @@
 const router = require("express").Router();
 const authMiddleware = require("../../middlewares/tokenValidator");
-const missingToken = require("../../middlewares/misingToken");
 const createAdmin = require("../../components/admin/create");
 const deleteAdmin = require("../../components/admin/delete");
 const readAdmin = require("../../components/admin/read");
 const updateAdmin = require("../../components/admin/update");
 const viewAdmin = require("../../components/admin/view");
-router.post("/", createAdmin.createAdmin);
-router.put("/:id", updateAdmin.updateAdmin);
-router.delete("/:id", deleteAdmin.deleteAdmin);
-router.get("/", readAdmin.readAdmin);
-router.get("/:id", viewAdmin.viewAdmin);
+router.post("/",authMiddleware.authorize, createAdmin.createAdmin);
+router.put("/:id",authMiddleware.authorize, updateAdmin.updateAdmin);
+router.delete("/:id",authMiddleware.authorize, deleteAdmin.deleteAdmin);
+router.get("/",authMiddleware.authorize, readAdmin.readAdmin);
+router.get("/:id",authMiddleware.authorize, viewAdmin.viewAdmin);
 
 module.exports = router;
--- backend/src/routes/colaborador/colaborador.js
+++ backend/src/routes/colaborador/colaborador.js
@@ -6,10 +6,18 @@ const deleteColaborador = require("../../components/colaborador/delete");
 const readColaborador = require("../../components/colaborador/read");
 const updateColaborador = require("../../components/colaborador/update");
 const viewColaborador = require("../../components/colaborador/view");
-router.post("/", createColaborador.createColaborador);
-router.put("/:id", updateColaborador.updateColaborador);
-router.delete("/:id", deleteColaborador.deleteColaborador);
-router.get("/", readColaborador.readColaborador);
-router.get("/:id", viewColaborador.viewColaborador);
+router.post("/", authMiddleware.authorize, createColaborador.createColaborador);
+router.put(
+  "/:id",
+  authMiddleware.authorize,
+  updateColaborador.updateColaborador
+);
+router.delete(
+  "/:id",
+  authMiddleware.authorize,
+  deleteColaborador.deleteColaborador
+);
+router.get("/", authMiddleware.authorize, readColaborador.readColaborador);
+router.get("/:id", authMiddleware.authorize, viewColaborador.viewColaborador);
 
 module.exports = router;
